f1d85dcc679f45c429db5887bcd09c6487898bd1
nikolaus-albinger <89982526+nikolaus-albinger@users.noreply.github.com>
2021-12-08 15:01:30 +0100

    1.0

--- init.lua
+++ init.lua
@@ -8,5 +8,15 @@ minetest.register_node("graystone:light_gray_block", {
 minetest.register_craft({
     type = "shapeless",
     output = "graystone:light_gray_block",
-    recipe = { "dye:grey", "default:stone" },
+    recipe = { "dye:grey", "default:stonebrick" },
 })
+
+
+if minetest.get_modpath("moreblocks") then
+    stairsplus:register_all("moreblocks", "graystone:light_gray_block", "graystone:light_gray_block", {
+        description = "Light Gray Stone Brick",
+        tiles = {"graystone.png"},
+        groups = {cracky = 3},
+        sounds = default.node_sound_stone_defaults(),
+    })
+end
