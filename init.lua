9d4500f34c5057e483032cac911b1bb96c90c676
Niklp <89982526+Niklp09@users.noreply.github.com>
2022-06-05 22:09:09 +0200

    updates

--- init.lua
+++ init.lua
@@ -20,3 +20,42 @@ if minetest.get_modpath("moreblocks") then
         sounds = default.node_sound_stone_defaults(),
     })
 end
+
+minetest.register_node("graystone:brick", {
+	description = "Brick",
+	tiles = {"graystone_brick.png"},
+	paramtype2 = "facedir",
+	groups = {cracky=2, crumbly=2, choppy=2},
+	is_ground_content = false,
+	sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_craft({
+	output = "graystone:brick",
+	recipe = {
+		{"default:clay_brick", "default:clay_brick"},
+		{"basic_materials:wet_cement", "default:clay_brick"},
+	},
+})
+
+minetest.register_node("graystone:smooth_stone", {
+    description = S("Smooth Stone"),
+    tiles = {"smoothstone.png"},
+    groups = {cracky = 2, stone = 1},
+    sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_craft({
+	type = "cooking",
+	output = "graystone:smooth_stone",
+	recipe = "default:stone",
+})
+
+if minetest.get_modpath("moreblocks") then
+    stairsplus:register_all("moreblocks", "graystone:smooth_stone", "graystone:smooth_stone", {
+        description = "Light Gray Stone Brick",
+        tiles = {"smoothstone.png"},
+        groups = {cracky = 2, stone =1},
+        sounds = default.node_sound_stone_defaults(),
+    })
+end
